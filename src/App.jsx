43d12e6631e92df5ccb00bfcc65594f273868bd1
Gareth Chandler <gchand05@me.com>
2024-10-05 10:38:00 +1000

    Added router and home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,13 @@
 import "./App.css";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "./Home";
 
 function App() {
   return (
-    <Router>
-      <div>
-        <h1>Hello World</h1>
-        <Switch>
-          <Route path="/">
-            <Home />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route />
+    </Routes>
   );
 }
 
