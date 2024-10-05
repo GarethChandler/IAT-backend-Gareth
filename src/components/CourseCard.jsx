bf0f2a98a2e90af97934711d9510c2d3c6f9eaed
Gareth Chandler <gchand05@me.com>
2024-10-06 10:52:22 +1100

    Added course.map to inject course information into the component

--- src/components/CourseCard.jsx
+++ src/components/CourseCard.jsx
@@ -1,20 +1,29 @@
 import "../css/CourseCard.css";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
 
-const CourseCard = () => {
+const CourseCard = ({ courseName, imageUrl, description, timeToComplete }) => {
   return (
     <div className="courseCardInfo">
-      <img src="" alt="" />
+      <img src={imageUrl} alt={courseName} />
       <div className="courseCardText">
-        <h2>Course Name</h2>
-        <p>Some details about the course</p>
-        <p>50 hours to complete</p>
+        <h2>{courseName}</h2>
+        <p>{description}</p>
+        <p>{timeToComplete}</p>
       </div>
       <Link to="/details">
         <button>See details</button>
       </Link>
     </div>
   );
+};
+
+CourseCard.propTypes = {
+  courseName: PropTypes.string.isRequired,
+  imageUrl: PropTypes.string.isRequired,
+  detailsUrl: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  timeToComplete: PropTypes.string.isRequired,
 };
 
 export default CourseCard;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,50 @@
-import { useState } from "react";
+//import { useState } from "react";
 import CourseCard from "../components/CourseCard";
 import "../css/CourseCard.css";
 
 const Home = () => {
   //   const [count, setCount] = useState(0);
+
+  // Courses to display
+  const courses = [
+    {
+      courseName: "Backend Development",
+      imageUrl: "../src/assets/book-with-green-board-background_1150-3836.jpg",
+      description:
+        "This courees will cover the fundamentals of backend development using the MREN stack.",
+      timeToComplete: "7 weeks",
+    },
+    {
+      courseName: "Frontend Development",
+      imageUrl: "../src/assets/book-with-green-board-background_1150-3836.jpg",
+      description:
+        "This courees will cover the fundamentals of frontend development using the React.",
+      timeToComplete: "7 weeks",
+    },
+    {
+      courseName: "Frontend Development",
+      imageUrl: "../src/assets/book-with-green-board-background_1150-3836.jpg",
+      description:
+        "This courees will cover the fundamentals of frontend development using the React.",
+      timeToComplete: "7 weeks",
+    },
+    // {
+    //   courseName: "",
+    //   imgURL: "",
+    //   description: "",
+    //   timeToComplete: "",
+    // },
+  ];
+
   return (
     <>
       <div>
         <h1>MicroCourses</h1>
         <h2>Online Education</h2>
+        <p>
+          At MicroCourses we offer a diverse range of online courses for
+          learners worldwide
+        </p>
         <div className="card">
           {/* <button onClick={() => setCount((count) => count + 1)}>
             count is {count}
@@ -16,9 +52,16 @@ const Home = () => {
           <h3>Available Courses</h3>
           <p className="read-the-docs">Select a course to see details</p>
           <div>
-            <CourseCard />
-            <CourseCard />
-            <CourseCard />
+            {courses.map((course, index) => (
+              <CourseCard
+                key={index}
+                courseName={course.courseName}
+                imageUrl={course.imageUrl}
+                detailsUrl={course.detailsUrl}
+                description={course.description}
+                timeToComplete={course.timeToComplete}
+              />
+            ))}
           </div>
         </div>
       </div>
