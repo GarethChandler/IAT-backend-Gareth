9cc823dbb3e6663e9736c924389aaaec00a13afe
Gareth Chandler <gchand05@me.com>
2024-10-20 15:33:01 +1100

    Added mongo schema and model. Test postamn successful

--- Component2/index.js
+++ Component2/index.js
@@ -1,27 +1,61 @@
-import express from 'express';
-import mongoose from 'mongoose';
+// Import dependencies for Express and Mongoose.
+import express from "express";
+import mongoose from "mongoose";
 
+// refactor express to allow params and set port number
 const app = express();
 const PORT = 3000;
-//const mongoose = require('mongoose');
 
+// Establish connection to MongoDB with uri. Set try/catch for error handling.
 async function main() {
-    const uri = "mongodb+srv://glchand05:2SUmEzhsY24TF5bx@iatcluster.jqt04.mongodb.net/IAT_microservices?retryWrites=true&w=majority&appName=IATCluster";
-    try {
-        await mongoose.connect(uri);
-        const db = mongoose.connection.db;
-        const dbName = db.databaseName;
-        console.log(`Connected to MongoDB Atlas using Mongoose! Database Name: ${dbName}`);
+  const uri =
+    "mongodb+srv://glchand05:2SUmEzhsY24TF5bx@iatcluster.jqt04.mongodb.net/IAT_microservices?retryWrites=true&w=majority&appName=IATCluster";
+  try {
+    await mongoose.connect(uri);
+    const db = mongoose.connection.db;
+    const dbName = db.databaseName;
+    console.log(
+      `Connected to MongoDB Atlas using Mongoose! Database Name: ${dbName}`
+    );
 
-        const collections = await db.listCollections().toArray();
-        console.log(`Database Info: ${dbName} has ${collections.length} collections`);
-
-    } catch (e) {
-        console.error(e);
-    }
+    const collections = await db.listCollections().toArray();
+    console.log(
+      `Database Info: ${dbName} has ${collections.length} collections`
+    );
+    console.log("Collections");
+    collections.forEach((collection) => {
+      console.log(collection.name);
+    });
+  } catch (e) {
+    console.error(e);
+  }
 }
 main().catch(console.error);
 
+// Define MongoDB schema
+const courseSchema = new mongoose.Schema({
+  _id: mongoose.Schema.Types.ObjectId,
+  course_id: String,
+  courseName: String,
+  imageUrl: String,
+  description: String,
+  timeToComplete: String,
+});
+
+// Define mongo model
+const Course = mongoose.model("Course", courseSchema);
+
+// Get all courses
+app.get("/courses", async (req, res) => {
+  try {
+    const courses = await Course.find();
+    res.json(courses);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+});
+
+// Run server on port.
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
